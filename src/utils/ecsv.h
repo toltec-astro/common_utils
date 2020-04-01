e0f2941c13a7ac88d951742e4b8e1a04f417883b
Jerry Ma <jerry.ma.nk@gmail.com>
2020-04-01 09:21:11 -0400

    ecsv handle meta data from astropy

--- src/utils/ecsv.h
+++ src/utils/ecsv.h
@@ -1,11 +1,88 @@
 #pragma once
 
 #include <algorithm>
+#include <cassert>
 #include <complex>
 #include <regex>
 #include <sstream>
 #include <string_view>
+#include <type_traits>
+#include <variant>
 #include <yaml-cpp/yaml.h>
+
+namespace YAML {
+
+template <typename... Ts> struct convert<std::variant<Ts...>> {
+
+    using variant_t = std::variant<Ts...>;
+
+    /// @brief Recursively try convert node to variant alternative *in order*.
+    /// The order of types matters here, for example,
+    /// variant<string, int> will capture all values as string because
+    /// in YAML/Json everything is string.
+    /// for variant<int, string>, all nodes convertable to int are converted.
+    template <std::size_t i = 0>
+    static auto _to_variant(const YAML::Node &n) -> std::optional<variant_t> {
+        if constexpr (i < std::variant_size_v<variant_t>) {
+            using vt = std::variant_alternative_t<i, variant_t>;
+            std::optional<vt> result{};
+            if constexpr (std::is_same_v<vt, std::monostate>) {
+                // capture null with this
+                if (n.IsNull()) {
+                    result = std::monostate{};
+                }
+            } else {
+                if (vt v{}; YAML::convert<vt>::decode(n, v)) {
+                    result = v;
+                }
+            }
+            return result.has_value() ? std::move(result.value())
+                                      : _to_variant<i + 1>(n);
+        }
+        return std::nullopt;
+    }
+
+    static bool decode(const Node &node, variant_t &rhs) {
+        // capture null with monostate
+        if (node.IsNull()) {
+
+            if constexpr (std::bool_constant<(
+                              std::is_same_v<std::monostate, Ts> ||
+                              ...)>::value) {
+                rhs = std::monostate{};
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (auto opt_result = _to_variant(node); opt_result.has_value()) {
+            rhs = std::move(opt_result.value());
+            return true;
+        }
+        return false;
+    }
+};
+
+template <typename T> struct convert<std::optional<T>> {
+    using optional_t = std::optional<T>;
+
+    /// @brief Decode YAML node to optional.
+    /// Note that in YAML/Json everything is string.
+    /// optional<string> will capture all node.
+    static bool decode(const Node &node, optional_t &rhs) {
+        if (node.IsNull()) {
+            rhs = std::nullopt;
+            return true;
+        }
+        if (T v{}; convert<T>::decode(node, v)) {
+            rhs = std::move(v);
+            return true;
+        }
+        return false;
+    }
+};
+
+} // namespace YAML
 
 namespace ecsv {
 
@@ -27,6 +104,18 @@ constexpr auto apply_const_sequence(Func &&f, S<T, Is...>) -> decltype(auto) {
     return std::forward<Func>(f)(std::integral_constant<T, Is>{}...);
 }
 
+// check if type is template instance
+template <typename, template <typename...> typename = std::void_t,
+          template <typename...> typename = std::void_t>
+struct is_instance : public std::false_type {};
+
+template <typename... Ts, template <typename...> typename T>
+struct is_instance<T<Ts...>, T> : public std::true_type {};
+
+template <typename... Ts, template <typename...> typename T,
+          template <typename...> typename U, typename... Us>
+struct is_instance<T<U<Us...>, Ts...>, T, U> : public std::true_type {};
+
 } // namespace internal
 
 namespace spec {
@@ -36,6 +125,7 @@ static constexpr char ECSV_DELIM_CHAR = ' ';
 static constexpr std::string_view ECSV_HEADER_PREFIX = "# ";
 static constexpr std::string_view ECSV_VERSION_LINE_REGEX = "^# %ECSV .+";
 static constexpr std::string_view ECSV_VERSION_LINE_PREFIX = "%ECSV ";
+static constexpr std::string_view ECSV_META_TAG = "tag:yaml.org,2002:omap";
 static constexpr std::string_view k_datatype = "datatype";
 static constexpr std::string_view k_meta = "meta";
 static constexpr std::string_view k_name = "name";
@@ -269,6 +359,85 @@ void dump_header(OStream &os, std::vector<std::string> colnames,
         header[k_meta] = std::move(meta);
     }
     spec::dump_yaml_header(os, header);
+}
+
+/// @brief Create meta node from map-like data structure.
+template <typename Map> auto map_to_meta(Map &&map) {
+    YAML::Node n;
+    using key_t = typename std::decay_t<Map>::key_type;
+    using value_t = typename std::decay_t<Map>::mapped_type;
+    for (const auto &item : std::forward<decltype(map)>(map)) {
+        // handle value types
+        if constexpr (internal::is_instance<value_t, std::variant>::value) {
+            std::visit(
+                [&](const auto &v) {
+                    using vt = std::decay_t<decltype(v)>;
+                    if constexpr (std::is_same_v<vt, std::monostate>) {
+                        n.push_back(YAML::Null);
+                    } else {
+                        n.push_back(std::map{std::pair{item.first, v}});
+                    };
+                },
+                item.second);
+        } else {
+            n.push_back(std::map{std::pair{item.first, item.second}});
+        }
+    };
+    n.SetTag(std::string{spec::ECSV_META_TAG});
+    return n;
+}
+
+/// @brief Create map from meta, if viable.
+/// @param meta The meta.
+/// @param rest A node to capture any uncaptured entries in meta.
+/// The map key type must be int-like or string, and the mapped type can be
+/// int-like, floating point, string, std::optional, or std::variant
+template <typename key_t, typename value_t>
+auto meta_to_map(YAML::Node meta, YAML::Node *rest = nullptr)
+    -> std::map<key_t, value_t> {
+
+    using item_t = std::pair<key_t, value_t>;
+    auto decode_item = [](const auto &n) -> std::optional<item_t> {
+        item_t item{};
+        if (!YAML::convert<key_t>::decode(n.first, item.first)) {
+            return std::nullopt;
+        }
+        if (!YAML::convert<value_t>::decode(n.second, item.second)) {
+            return std::nullopt;
+        }
+        return std::move(item);
+    };
+    std::map<key_t, value_t> result;
+    YAML::Node rest_{}; // this is to collect any uncaptured item
+    rest_.SetTag(meta.Tag());
+    if (meta.IsSequence() && (meta.Tag() == spec::ECSV_META_TAG)) {
+        // ordered map
+        for (const auto &v : meta) {
+            assert(v.IsMap() && v.size() == 1);
+            const auto it = v.begin();
+            if (auto opt_item = decode_item(*it); opt_item) {
+                result[opt_item.value().first] = opt_item.value().second;
+            } else if (rest != nullptr) {
+                rest_.push_back(v);
+            }
+        }
+    } else if (meta.IsMap()) {
+        for (const auto &v : meta) {
+            if (auto opt_item = decode_item(v); opt_item) {
+                result[opt_item.value().first] = opt_item.value().second;
+            } else if (rest != nullptr) {
+                rest_.operator[](v.first) = v.second;
+            }
+        }
+    } else {
+        if (rest != nullptr) {
+            rest_.reset(std::move(meta));
+        }
+    }
+    if (rest != nullptr) {
+        rest->reset(std::move(rest_));
+    }
+    return result;
 }
 
 } // namespace ecsv
