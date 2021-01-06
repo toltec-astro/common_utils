a6d12c79a6b81dd08e789c471a7c63b3060858d0
Jerry Ma <jerry.ma.nk@gmail.com>
2021-01-06 14:32:46 -0500

    update yamlconfig

--- src/utils/yamlconfig.h
+++ src/utils/yamlconfig.h
@@ -11,6 +11,11 @@ namespace config {
  */
 struct YamlConfig {
 
+    template <typename U>
+    using is_validate_key_t =
+        std::is_invocable<decltype((YAML::Node(YAML::Node::*)(U)) &
+                                   YAML::Node::operator[]),
+                          YAML::Node, U>;
     using value_t = YAML::Node;
     using storage_t = YAML::Node;
     YamlConfig() = default;
@@ -24,32 +29,29 @@ struct YamlConfig {
         return os << config.dump_to_str();
     }
 
-    template <typename T, typename KT> auto get_typed(KT &&key) const {
-        return operator[](FWD(key)).template as<T>();
-    }
-    template <typename T, typename KT>
-    auto get_typed(KT &&key, T &&defval) const {
-        decltype(auto) node = multiget_node(FWD(key));
-        if (node.IsDefined() && !node.IsNull()) {
-            return node.template as<T>();
+    template <typename key_t> decltype(auto) operator[](key_t &&key) const {
+        auto multiget_node_impl = meta::y_combinator(
+            [](auto &&get_node, auto &&node, auto &&x, auto &&...rest) {
+                if constexpr (sizeof...(rest) == 0) {
+                    return FWD(node)[FWD(x)];
+                } else {
+                    return get_node(FWD(node)[FWD(x)], rest...);
+                }
+            });
+        if constexpr (meta::is_instance<key_t, std::tuple>::value) {
+            return std::apply(multiget_node_impl,
+                              std::tuple_cat(std::tuple{m_node}, FWD(key)));
+        } else if constexpr (is_validate_key_t<key_t>::value) {
+            return std::apply(multiget_node_impl,
+                              std::make_tuple(m_node, FWD(key)));
         }
-        return FWD(defval);
     }
 
-    template <typename KT> auto get_str(const KT &key) const {
-        return get_typed<std::string, KT>(key);
-    }
-    template <typename KT>
-    auto get_str(const KT &key, const std::string &defval) const {
-        return get_typed<std::string, KT>(key, std::string(defval));
+    template <typename key_t> bool has(key_t &&key) const {
+        return operator[](FWD(key)).IsDefined();
     }
 
-    template <typename KT> decltype(auto) operator[](KT &&key) const {
-        return multiget_node(FWD(key));
-    }
-
-    bool has(const key_t &key) const { return m_node[key].IsDefined(); }
-    template <typename T> bool has_typed(const key_t &key) const {
+    template <typename T, typename key_t> bool has_typed(key_t &&key) const {
         if (!has(key)) {
             return false;
         }
@@ -61,32 +63,30 @@ struct YamlConfig {
         return true;
     }
 
+    template <typename T, typename key_t> auto get_typed(key_t &&key) const {
+        return operator[](FWD(key)).template as<T>();
+    }
+    template <typename T, typename key_t>
+    auto get_typed(key_t &&key, T &&defval) const {
+        decltype(auto) node = operator[](FWD(key));
+        if (node.IsDefined() && !node.IsNull()) {
+            return node.template as<T>();
+        }
+        return FWD(defval);
+    }
+
+    template <typename... Args> auto get_str(Args &&...args) const {
+        return get_typed<std::string>(FWD(args)...);
+    }
+
     std::string pformat() const { return dump_to_str(); }
+
+    static auto from_filepath(const std::string &filepath) {
+        return YamlConfig(YAML::LoadFile(filepath));
+    }
 
 private:
     storage_t m_node;
-
-    template <typename KT> decltype(auto) multiget_node(KT &&key) {
-        auto multiget_node_impl = meta::y_combinator(
-            [](auto &&get_node, auto &&node, auto &&x, auto &&...rest) {
-                if constexpr (sizeof...(rest) == 0) {
-                    return FWD(node)[FWD(x)];
-                } else {
-                    return get_node(FWD(node)[FWD(x)], rest...);
-                }
-            });
-        if constexpr (meta::is_instance<KT, std::tuple>::value) {
-            return std::apply(multiget_node_impl,
-                              std::tuple_cat(std::tuple{m_node}, FWD(key)));
-        } else if (meta::is_instance<KT, std::initializer_list>::value) {
-            return std::apply(
-                multiget_node_impl,
-                std::tuple_cat(std::tuple{m_node},
-                               meta::list_to_tuple_t<KT>{FWD(key)}));
-        } else {
-            return std::apply(multiget_node_impl, std::tuple{m_node, FWD(key)});
-        }
-    }
 };
 
 } // namespace config
